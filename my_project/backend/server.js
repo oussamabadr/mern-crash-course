1618ef4e137e431e0391809329994598797b8d51
Oussama <oussma.badr@agil-it.fr>
2025-04-28 02:07:05 +0200

    ready for deployment

--- my_project/backend/server.js
+++ my_project/backend/server.js
@@ -1,18 +1,31 @@
-import express from 'express';
-import { connectDB } from './config/db.js';
-import productRoutes from './routes/product.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
+if (process.env.APP_ENV === "production") {
+    console.log("App is runninng in Production env.");
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+} else {
+    console.log("App is runninng in Development env.");
+}
+
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-// sgVHcRWo4Yd7UxFb 
